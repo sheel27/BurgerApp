69ecbce93c26679da35014fd4cbf00b14001e3bb
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2024-01-21 17:18:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 //Added
 app.use(cors(
